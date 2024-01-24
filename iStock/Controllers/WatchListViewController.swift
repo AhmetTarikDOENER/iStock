f9a4e0f99d31ef82f9129b639e008381aee2e9a9
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-01-24 16:48:32 +0300

    Started to create result view contoller which responsible for searching.

--- iStock/Controllers/WatchListViewController.swift
+++ iStock/Controllers/WatchListViewController.swift
@@ -12,7 +12,32 @@ class WatchListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setupSearchController()
+    }
+    
+    private func setupSearchController() {
+        let resultViewController = SearchResultsViewController()
+        let searchViewController = UISearchController(searchResultsController: resultViewController)
+        searchViewController.searchResultsUpdater = self
+        navigationItem.searchController = searchViewController
     }
 
 }
 
+extension WatchListViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let resultsVC = searchController.searchResultsController as? SearchResultsViewController,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            
+            return
+        }
+        // Optimize to reduce nimber og searches for when user stops typing.
+        
+        // Call API to search
+        
+        // Update resultsViewController
+    }
+    
+}
